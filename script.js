737c1acf13e70c5393a5e40cd38587160469da41
suryanagireddy10 <nagireddysurya0@gmail.com>
2024-12-03 12:22:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Welcome to Surya Nagireddy's Website!");
+document.addEventListener('DOMContentLoaded', function () {
+    const greeting = document.getElementById('greeting');
+    const currentTime = new Date().getHours();
+
+    if (currentTime < 12) {
+        greeting.innerText = 'Good Morning! I\'m Surya Nagireddy, a passionate web developer.';
+    } else if (currentTime < 18) {
+        greeting.innerText = 'Good Afternoon! I\'m Surya Nagireddy, a passionate web developer.';
+    } else {
+        greeting.innerText = 'Good Evening! I\'m Surya Nagireddy, a passionate web developer.';
+    }
+});
